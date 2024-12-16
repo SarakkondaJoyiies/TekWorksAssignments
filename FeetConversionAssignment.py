af7be53857b28f374ee663850cf089c43dd388e0
SarakkondaJoyiies <167058331+SarakkondaJoyiies@users.noreply.github.com>
2024-12-16 14:55:36 +0530

    Add files via upload

---
+++ FeetConversionAssignment.py
@@ -0,0 +1,8 @@
+feet_val = float(input("Enter the distance in feet:"))
+inches = feet_val * 12
+yards = feet_val / 3
+miles = feet_val / 5280
+print("Distance in feet:", feet_val)
+print("Distance in inches:", inches)
+print("Distance in yards:", yards)
+print("Distance in miles:",miles)
